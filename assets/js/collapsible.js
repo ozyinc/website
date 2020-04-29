ae484b6eafbd2dc99eef6529d742063c09ac4b47
Ozan İncesulu <ozyinc@users.noreply.github.com>
2020-04-29 14:27:56 +0300

    refactor: use for..of syntax for all iterations over DOM

--- assets/js/collapsible.js
+++ assets/js/collapsible.js
@@ -1,8 +1,6 @@
-const coll = document.getElementsByClassName("collapsible");
-let i;
 
-for (i = 0; i < coll.length; i++) {
-    coll[i].addEventListener("click", function () {
+for (const collapsible of document.getElementsByClassName("collapsible")) {
+    collapsible.addEventListener("click", function () {
         this.classList.toggle("active");
         const content = this.parentElement.nextElementSibling;
         if (content.style.maxHeight) {
@@ -14,17 +12,17 @@ for (i = 0; i < coll.length; i++) {
 }
 
 const cvDetailed = window.location.href.includes("detailed");
-Array.from(document.getElementsByClassName("detail")).map(elem => {
-    elem.style.display = cvDetailed ? "block" : "none";
-})
+for (const detailsElement of document.getElementsByClassName("detail")) {
+    detailsElement.style.display = cvDetailed ? "block" : "none";
+}
 const detailIds = {};
-Array.from(document.getElementsByClassName("detail-link")).map(elem => {
-    elem.style.display = "none";
-    if(!cvDetailed && !detailIds[elem.id]) {
-        detailIds[elem.id] = 1;
-        elem.style.display = "block";
+for (const detailsLink of document.getElementsByClassName("detail-link")) {
+    detailsLink.style.display = "none";
+    if(!cvDetailed && !detailIds[detailsLink.id]) {
+        detailIds[detailsLink.id] = 1;
+        detailsLink.style.display = "block";
     }
-})
+}
 
 if (cvDetailed) {
     document.getElementById("cv-link-detailed").classList.add("selected");
@@ -32,7 +30,7 @@ if (cvDetailed) {
     document.getElementById("cv-link-basic").classList.add("selected");
 }
 
-for (let link of document.links) {
+for (const link of document.links) {
     if (link.hostname !== window.location.hostname) {
         link.target = "_blank";
         link.rel = "nofollow,noopener"
